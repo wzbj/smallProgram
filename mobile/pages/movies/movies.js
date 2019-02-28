f70f293696b106d2515b4a64d737639a238ed3d7
wzbj <907403638@qq.com>
2019-02-28 23:19:28 +0800

    接口请求

--- mobile/pages/movies/movies.js
+++ mobile/pages/movies/movies.js
@@ -11,7 +11,19 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    wx.request({
+      url: 'https://t.yushu.im/v2/movie/top250', // 仅为示例，并非真实的接口地址
+      // data: {
+      //   x: '',
+      //   y: ''
+      // },
+      header: {
+        'content-type': '' // 默认值
+      },
+      success(res) {
+        console.log(res.data)
+      }
+    })
   },
 
   /**
